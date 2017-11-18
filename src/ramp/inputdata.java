28d5ca51640d8aaa2443f973a35a24d52d435692
Bishop <jhs3920@wmich.edu>
2017-11-18 12:14:42 -0500

    adding more comboboxes

--- src/ramp/inputdata.java
+++ src/ramp/inputdata.java
@@ -7,29 +7,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.Font;
-import java.awt.FlowLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.SpringLayout;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.JTabbedPane;
 
 public class inputdata extends JFrame {
 
 	private JPanel contentPane;
-	private JComboBox<Integer> comboFeet_1;
-	private JComboBox<Integer> comboInches;
-	private JComboBox<String> comboInParts;
-	private JPanel panelNoTurns;
-	private JPanel panel;
-	private JPanel panel_1;
-	private JLabel lblLayout;
 
 	/**
 	 * Launch the application.
@@ -51,72 +32,96 @@ public class inputdata extends JFrame {
 	 * Create the frame.
 	 */
 	public inputdata() {
+		setTitle("RampDesign by Lana-Wulf Soft");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 799, 601);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		JLabel lblHeightOfDeck = new JLabel("Height of Deck");
+		lblHeightOfDeck.setBounds(10, 10, 92, 19);
 		lblHeightOfDeck.setFont(new Font("Dialog", Font.PLAIN, 14));
+		contentPane.add(lblHeightOfDeck);
 		
 		JComboBox<Integer> comboFeet = setFeet();
-		
-		JLabel labelFeet = new JLabel("'");
-		labelFeet.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		comboFeet.setBounds(112, 10, 29, 22);
 		
 		JComboBox<Integer> comboInch = setInches();
+		comboInch.setBounds(661, 10, 29, 22);
 				
 		JComboBox<String> comboInPart = setInParts();
+		comboInPart.setBounds(203, 10, 29, 22);
+		
 		
 		JLabel label_1 = new JLabel("''");
+		label_1.setBounds(242, 11, 6, 17);
 		label_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.UNRELATED_GAP_COLSPEC,
-				ColumnSpec.decode("220px:grow"),
-				ColumnSpec.decode("32px"),
-				FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
-				ColumnSpec.decode("3px"),
-				FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
-				ColumnSpec.decode("32px"),
-				FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
-				ColumnSpec.decode("32px"),
-				FormSpecs.LABEL_COMPONENT_GAP_COLSPEC,
-				ColumnSpec.decode("10px"),},
-			new RowSpec[] {
-				FormSpecs.PARAGRAPH_GAP_ROWSPEC,
-				RowSpec.decode("24px"),
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("default:grow"),}));
-		contentPane.add(lblHeightOfDeck, "2, 2, left, center");
-		contentPane.add(comboFeet_1, "3, 2, left, top");
-		contentPane.add(labelFeet, "5, 2, left, center");
-		contentPane.add(comboInches, "7, 2, left, top");
-		contentPane.add(comboInParts, "9, 2, left, top");
-		contentPane.add(label_1, "11, 2, left, center");
+		contentPane.setLayout(null);
 		
-		lblLayout = new JLabel("Layout");
-		lblLayout.setFont(new Font("Dialog", Font.PLAIN, 14));
-		contentPane.add(lblLayout, "2, 4");
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		contentPane.add(tabbedPane, "2, 6, 9, 1, fill, fill");
+		contentPane.add(comboFeet);
 		
-		panelNoTurns = new JPanel();
-		tabbedPane.addTab("No Turns", null, panelNoTurns, null);
+		JLabel labelFeet = new JLabel("'");
+		labelFeet.setBounds(151, 11, 3, 17);
+		labelFeet.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(labelFeet);
+		contentPane.add(comboInch);
+		contentPane.add(comboInPart);
+		contentPane.add(label_1);
 		
-		panel = new JPanel();
-		tabbedPane.addTab("One Turn", null, panel, null);
+		JLabel lblDimentionsOfDeck = new JLabel("Dimentions of Deck");
+		lblDimentionsOfDeck.setFont(new Font("Dialog", Font.PLAIN, 14));
+		lblDimentionsOfDeck.setBounds(336, 12, 131, 14);
+		contentPane.add(lblDimentionsOfDeck);
 		
-		panel_1 = new JPanel();
-		tabbedPane.addTab("Two Turns", null, panel_1, null);
+		JComboBox<Integer> comboDimFt1 = setFeet();
+		comboDimFt1.setBounds(477, 10, 29, 22);
+		contentPane.add(comboDimFt1);
+		
+		JComboBox<Integer> comboDimIn1 = setInches();
+		comboDimIn1.setBounds(529, 10, 29, 22);
+		contentPane.add(comboDimIn1);
+		
+		JLabel label = new JLabel("'");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label.setBounds(516, 14, 3, 17);
+		contentPane.add(label);
+		
+		JLabel lblX = new JLabel("''    X  ");
+		lblX.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblX.setBounds(568, 14, 43, 17);
+		contentPane.add(lblX);
+		
+		JComboBox<Integer> comboDimFt2 = setFeet();
+		comboDimFt2.setBounds(580, 10, 29, 22);
+		contentPane.add(comboDimFt2);
+		
+		JComboBox<Integer> comboDimIn2 = setInches();
+		comboDimIn2.setBounds(632, 10, 29, 22);
+		contentPane.add(comboDimIn2);
+		
+//		lblLayout = new JLabel("Layout");
+//		lblLayout.setBounds(10, 93, 43, 19);
+//		lblLayout.setFont(new Font("Dialog", Font.PLAIN, 14));
+//		contentPane.add(lblLayout);
+//		
+//		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+//		contentPane.add(tabbedPane);
+//		
+//		panelNoTurns = new JPanel();
+//		tabbedPane.addTab("No Turns", null, panelNoTurns, null);
+//		
+//		panel = new JPanel();
+//		tabbedPane.addTab("One Turn", null, panel, null);
+//		
+//		panel_1 = new JPanel();
+//		tabbedPane.addTab("Two Turns", null, panel_1, null);
 	}
 	
-	//**********set boxes*******************
+	//**********set combo boxes boxes*******************
 	public JComboBox<Integer> setFeet(){
-		comboFeet_1 = new JComboBox<Integer>();
+		JComboBox<Integer> comboFeet_1 = new JComboBox<Integer>();
 		for (int i = 0; i < 20; i++) {
 			comboFeet_1.addItem(i);
 		}
@@ -124,7 +129,7 @@ public class inputdata extends JFrame {
 	}
 	
 	public JComboBox<Integer> setInches(){
-		comboInches = new JComboBox<Integer>();
+		JComboBox<Integer> comboInches = new JComboBox<Integer>();
 		for (int i = 0; i < 12; i++) {
 			comboInches.addItem(i);
 		}
@@ -132,7 +137,7 @@ public class inputdata extends JFrame {
 	}
 	
 	public JComboBox<String> setInParts(){
-		comboInParts = new JComboBox<String>();
+		JComboBox<String> comboInParts = new JComboBox<String>();
 		comboInParts.addItem("1/8");
 		comboInParts.addItem("1/4");
 		comboInParts.addItem("3/8");
