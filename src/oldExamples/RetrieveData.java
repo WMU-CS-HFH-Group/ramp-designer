591bbd5e7c092764309da917cd4c50e7b8aa7814
Bishop <jhs3920@wmich.edu>
2017-11-18 14:00:06 -0500

    first line of gui complete

--- src/oldExamples/RetrieveData.java
+++ src/oldExamples/RetrieveData.java
@@ -1,11 +1,8 @@
 package oldExamples;
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -17,13 +14,9 @@ import java.awt.Font;
 import javax.swing.AbstractAction;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.Action;
-
-import java.awt.Cursor;
-
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
--- src/ramp/inputdata.java
+++ src/ramp/inputdata.java
@@ -7,6 +7,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.Font;
+import javax.swing.SpringLayout;
 
 public class inputdata extends JFrame {
 
@@ -38,68 +39,114 @@ public class inputdata extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
 		
 		JLabel lblHeightOfDeck = new JLabel("Height of Deck");
-		lblHeightOfDeck.setBounds(10, 10, 92, 19);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblHeightOfDeck, 10, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblHeightOfDeck, 10, SpringLayout.WEST, contentPane);
 		lblHeightOfDeck.setFont(new Font("Dialog", Font.PLAIN, 14));
 		contentPane.add(lblHeightOfDeck);
 		
-		JComboBox<Integer> comboFeet = setFeet();
-		comboFeet.setBounds(112, 10, 29, 22);
-		
-		JComboBox<Integer> comboInch = setInches();
-		comboInch.setBounds(661, 10, 29, 22);
-				
-		JComboBox<String> comboInPart = setInParts();
-		comboInPart.setBounds(203, 10, 29, 22);
-		
-		
-		JLabel label_1 = new JLabel("''");
-		label_1.setBounds(242, 11, 6, 17);
-		label_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		contentPane.setLayout(null);
-		
-		
+		JComboBox<Integer> comboFeet = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboFeet, 0, SpringLayout.NORTH, lblHeightOfDeck);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboFeet, 6, SpringLayout.EAST, lblHeightOfDeck);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboFeet, 49, SpringLayout.EAST, lblHeightOfDeck);
+		setFeet(comboFeet);
 		contentPane.add(comboFeet);
 		
 		JLabel labelFeet = new JLabel("'");
-		labelFeet.setBounds(151, 11, 3, 17);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, labelFeet, 0, SpringLayout.NORTH, comboFeet);
+		sl_contentPane.putConstraint(SpringLayout.WEST, labelFeet, 3, SpringLayout.EAST, comboFeet);
 		labelFeet.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(labelFeet);
+		
+		JComboBox<Integer> comboInch = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboInch, 0, SpringLayout.NORTH, labelFeet);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboInch, 6, SpringLayout.EAST, labelFeet);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboInch, 49, SpringLayout.EAST, labelFeet);
+		setInches(comboInch);
 		contentPane.add(comboInch);
-		contentPane.add(comboInPart);
-		contentPane.add(label_1);
+				
+		JComboBox<String> comboInPart = new JComboBox<String>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboInPart, 0, SpringLayout.NORTH, lblHeightOfDeck);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboInPart, 8, SpringLayout.EAST, comboInch);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboInPart, 59, SpringLayout.EAST, comboInch);
+		setInParts(comboInPart);
+		contentPane.add(comboInPart);		
+		
+		JLabel labelIn = new JLabel("''");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, labelIn, 0, SpringLayout.NORTH, comboInPart);
+		sl_contentPane.putConstraint(SpringLayout.WEST, labelIn, 3, SpringLayout.EAST, comboInPart);
+		labelIn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(labelIn);
 		
 		JLabel lblDimentionsOfDeck = new JLabel("Dimentions of Deck");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblDimentionsOfDeck, 12, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblDimentionsOfDeck, 336, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblDimentionsOfDeck, 26, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblDimentionsOfDeck, 467, SpringLayout.WEST, contentPane);
 		lblDimentionsOfDeck.setFont(new Font("Dialog", Font.PLAIN, 14));
-		lblDimentionsOfDeck.setBounds(336, 12, 131, 14);
 		contentPane.add(lblDimentionsOfDeck);
 		
-		JComboBox<Integer> comboDimFt1 = setFeet();
-		comboDimFt1.setBounds(477, 10, 29, 22);
+		JComboBox<Integer> comboDimFt1 = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboDimFt1, 0, SpringLayout.NORTH, lblDimentionsOfDeck);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboDimFt1, 6, SpringLayout.EAST, lblDimentionsOfDeck);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboDimFt1, 49, SpringLayout.EAST, lblDimentionsOfDeck);
+		setFeet(comboDimFt1);
 		contentPane.add(comboDimFt1);
 		
-		JComboBox<Integer> comboDimIn1 = setInches();
-		comboDimIn1.setBounds(529, 10, 29, 22);
+		JLabel lblDimFt1 = new JLabel("'");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblDimFt1, 0, SpringLayout.NORTH, comboDimFt1);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblDimFt1, 3, SpringLayout.EAST, comboDimFt1);
+		lblDimFt1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblDimFt1);
+		
+		JComboBox<Integer> comboDimIn1 = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboDimIn1, 0, SpringLayout.NORTH, lblDimFt1);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboDimIn1, 6, SpringLayout.EAST, lblDimFt1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboDimIn1, 49, SpringLayout.EAST, lblDimFt1);
+		setInches(comboDimIn1);
 		contentPane.add(comboDimIn1);
 		
-		JLabel label = new JLabel("'");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label.setBounds(516, 14, 3, 17);
-		contentPane.add(label);
+		JLabel lblDimIn1 = new JLabel("''    X  ");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblDimIn1, 0, SpringLayout.NORTH, comboDimIn1);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblDimIn1, 3, SpringLayout.EAST, comboDimIn1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblDimIn1, 49, SpringLayout.EAST, comboDimIn1);
+		lblDimIn1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblDimIn1);
 		
-		JLabel lblX = new JLabel("''    X  ");
-		lblX.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblX.setBounds(568, 14, 43, 17);
-		contentPane.add(lblX);
-		
-		JComboBox<Integer> comboDimFt2 = setFeet();
-		comboDimFt2.setBounds(580, 10, 29, 22);
+		JComboBox<Integer> comboDimFt2 = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboDimFt2, 0, SpringLayout.NORTH, lblDimIn1);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboDimFt2, 0, SpringLayout.EAST, lblDimIn1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboDimFt2, 49, SpringLayout.EAST, lblDimIn1);
+		setFeet(comboDimFt2);
 		contentPane.add(comboDimFt2);
 		
-		JComboBox<Integer> comboDimIn2 = setInches();
-		comboDimIn2.setBounds(632, 10, 29, 22);
+		JLabel lblDimFt2 = new JLabel("'");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblDimFt2, 0, SpringLayout.NORTH, comboDimFt2);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblDimFt2, 3, SpringLayout.EAST, comboDimFt2);
+		lblDimFt2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblDimFt2);
+		
+		JComboBox<Integer> comboDimIn2 = new JComboBox<Integer>();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, comboDimIn2, 0, SpringLayout.NORTH, lblDimFt2);
+		sl_contentPane.putConstraint(SpringLayout.WEST, comboDimIn2, 6, SpringLayout.EAST, lblDimFt2);
+		sl_contentPane.putConstraint(SpringLayout.EAST, comboDimIn2, 49, SpringLayout.EAST, lblDimFt2);
+		setInches(comboDimIn2);
 		contentPane.add(comboDimIn2);
+		
+		JLabel lblDimIn2 = new JLabel("''");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblDimIn2, 0, SpringLayout.NORTH, comboDimIn2);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblDimIn2, 3, SpringLayout.EAST, comboDimIn2);
+		lblDimIn2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblDimIn2);
+		
+		JLabel lblStyle = new JLabel("Ramp Style: ");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblStyle, 10, SpringLayout.SOUTH, lblHeightOfDeck);
+		lblStyle.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblStyle, 0, SpringLayout.WEST, lblHeightOfDeck);
+		contentPane.add(lblStyle);
 		
 //		lblLayout = new JLabel("Layout");
 //		lblLayout.setBounds(10, 93, 43, 19);
@@ -120,32 +167,26 @@ public class inputdata extends JFrame {
 	}
 	
 	//**********set combo boxes boxes*******************
-	public JComboBox<Integer> setFeet(){
-		JComboBox<Integer> comboFeet_1 = new JComboBox<Integer>();
+	public void setFeet(JComboBox<Integer> combo){
 		for (int i = 0; i < 20; i++) {
-			comboFeet_1.addItem(i);
+			combo.addItem(i);
 		}
-		return comboFeet_1;
 	}
 	
-	public JComboBox<Integer> setInches(){
-		JComboBox<Integer> comboInches = new JComboBox<Integer>();
-		for (int i = 0; i < 12; i++) {
-			comboInches.addItem(i);
+	public void setInches(JComboBox<Integer> combo){
+		for (int i = 0; i < 48; i++) {
+			combo.addItem(i);
 		}
-		return comboInches;
 	}
 	
-	public JComboBox<String> setInParts(){
-		JComboBox<String> comboInParts = new JComboBox<String>();
-		comboInParts.addItem("1/8");
-		comboInParts.addItem("1/4");
-		comboInParts.addItem("3/8");
-		comboInParts.addItem("1/2");
-		comboInParts.addItem("5/8");
-		comboInParts.addItem("3/4");
-		comboInParts.addItem("7/8");
-		return comboInParts;
+	public void setInParts(JComboBox<String> combo){
+		combo.addItem("1/8");
+		combo.addItem("1/4");
+		combo.addItem("3/8");
+		combo.addItem("1/2");
+		combo.addItem("5/8");
+		combo.addItem("3/4");
+		combo.addItem("7/8");
 	}
 }
 
